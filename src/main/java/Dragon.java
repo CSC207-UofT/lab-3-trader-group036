62a5abba86287cc2b4815f528bd480e2dda36e74
zhumengzhiren <york.chen@mail.utoronto.ca>
2021-09-27 19:38:20 -0400

    York's code original version.

---
+++ src/main/java/Dragon.java
@@ -0,0 +1,42 @@
+public class Dragon implements Drivable, Domesticatable, Tradable{
+    private String color;
+    private int damage;
+    private int maxSpeed;
+
+    public Dragon(String color, int damage, int maxSpeed) {
+        this.color = color;
+        this.damage = damage;
+        this.maxSpeed = 10000;
+    }
+
+    @Override
+    public String sound() {
+        return "HHHHHOOOOOO!";
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxSpeed += 10000;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed -= 10000;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public int getPrice() {
+        return 1000000;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Max speed: " + this.maxSpeed +")" + "(Color:" + this.color + ")" + "(damage:" + this.damage + ")";
+    }
+}
